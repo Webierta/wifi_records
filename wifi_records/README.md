4b50aa6e153222625c28148807c133daf300f699
webierta <webierta@gmail.com>
2021-04-28 12:55:01 +0200

    project start

---
+++ wifi_records/README.md
@@ -0,0 +1,3 @@
+# Wifi Records
+
+Alert when the device connects to an unregistered Wi-Fi network.
---
+++ wifi_records/android/app/src/main/kotlin/com/github/webierta/wifi_records/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.webierta.wifi_records
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
